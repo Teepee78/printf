e27f111d5a81fca641bdfd8487b3a7c483e0009c
Toluwalase Phillips <eniolatoluwalase78@gmail.com>
2022-05-17 14:18:06 +0100

    Create _printf function, add feature to calculate length of format

---
+++ _printf.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _printf - prints any character
+ * @format: a string of characters and specifiers
+ * Return: length of characters except null byte
+ */
+int _printf(const char *format, ...)
+{
+	/* declare variables, length, i */
+	int length = 0, i = 0;
+
+	while (format[i])
+	{
+		length++;
+		i++;
+	}
+
+	return (length);
+}
--- test.c
+++ test.c
@@ -1,1 +1,13 @@
 #include "main.h"
+#include <stdio.h>
+
+int main(void)
+{
+	int length;
+
+	length = _printf("five");
+
+	printf("Length of string is: %d\n", length);
+
+	return (0);
+}
