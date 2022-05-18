4fedc90c9574b0654dadf2564c6f977899c07a4c
Toluwalase Phillips <eniolatoluwalase78@gmail.com>
2022-05-18 12:40:37 +0100

    Fixed Unsigned_int

--- unsigned_int.c
+++ unsigned_int.c
@@ -4,15 +4,12 @@
  * unsigned_int - prints unsigned integers
  * @digit: integer to be printed
  */
- void unsigned_int(unsigned int digit)
- {
-	 if (digit == 0)
-	 {
-		 _putchar('0');
-	 }
-	 if (digit < 1)
-	 {
-		 digit = -digit;
-	 }
-	 print_int(digit);
- }
+void unsigned_int(unsigned int digit)
+{
+	/* if integer is greater than 0 */
+	if (digit / 10)
+	{
+		unsigned_int(digit / 10);
+	}
+	_putchar(digit % 10 + '0');
+}
